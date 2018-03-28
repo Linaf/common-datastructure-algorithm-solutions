ed8c48f19265e3d4b343d2d39a993de7dbb70b48
Musema Hassen <mhassen@dminc.com>
2018-03-27 23:28:50 -0400

    Handle NullPointer in searchNodeByKey

--- tree-algorithms/src/linkedlist/SearchNode.java
+++ tree-algorithms/src/linkedlist/SearchNode.java
@@ -7,8 +7,9 @@ public class SearchNode {
 	public static Node searchNodeByData(Node head, String key) {
 		Node temp=head;
 		while(temp!=null) {
-			if(temp.getData().getKey()==key)
-				return temp;
+			if(temp.getData()!=null)
+				if(temp.getData().getKey()==key)
+					return temp;
 			temp=temp.getNext();
 		}
 		
